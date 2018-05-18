23764c6fc78639b664a59bb546db0094179fcd00
karol <karolsiegied@gmail.com>
2018-05-18 23:22:13 +0200

    Uproszczenie metod zmiany przycisku glownego Menu

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -1,8 +1,7 @@
 from PyQt5 import QtWidgets
 from src.gui.newGui import Ui_MainWindow
 clicked = False
-lastButton = ""
-changeButton = False
+
 
 
 class GUI_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -10,44 +9,49 @@ class GUI_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(GUI_MainWindow, self).__init__(parent) #QtCore.Qt.FramelessWindowHint
         self.setupUi(self)
         self.functionButtons()
-        self.lastButtonObject = QtWidgets.QFrame()
+        self.lastButtonObject = QtWidgets.QFrame() # przechowuje obiekt poprzedniego przycisku menu
+        self.objectList = []
 
-        #lastButtonObject = ""
+
     def functionButtons(self):
         self.driveButton.pressed.connect(lambda: self.menuButtonChanged(self.vfMain))
         self.alertsButton.pressed.connect(lambda: self.menuButtonChanged(self.vfAlerts))
         self.statsButton.pressed.connect(lambda: self.menuButtonChanged(self.vfStats))
         self.settingsButton.pressed.connect(lambda: self.menuButtonChanged(self.vfSettings))
 
+
     def menuButtonChanged(self, widget):
-        global lastButton
-        global changeButton
+        self.objectList.append(widget)
         iconObject = widget.findChild(QtWidgets.QPushButton)
+        self.objectList.append(iconObject)
         textObject = widget.findChild(QtWidgets.QLabel)
+        self.objectList.append(textObject)
+        objectListCopy = self.objectList
 
         if not self.lastButtonObject.objectName():
             self.lastButtonObject = widget
-            changeButton = True
+            self.menuButtonAreaStyleUpdate(objectListCopy, True)
         else:
             if self.lastButtonObject.objectName() != widget.objectName():
-                self.menuButtonAreaStyleUpdate(self.lastButtonObject, False)
+                self.menuButtonAreaStyleUpdate(objectListCopy, True)
+                del self.objectList[:]              # wyczyszczenie listy obiektów
+
+                self.objectList.append(self.lastButtonObject)
                 oldIconObject = self.lastButtonObject.findChild(QtWidgets.QPushButton)
+                self.objectList.append(oldIconObject)
                 oldTextObject = self.lastButtonObject.findChild(QtWidgets.QLabel)
-                self.menuButtonAreaStyleUpdate(self.lastButtonObject, False)
-                self.menuButtonAreaStyleUpdate(oldIconObject, False)
-                self.menuButtonAreaStyleUpdate(oldTextObject, False)
+                self.objectList.append(oldTextObject)
+                objectListCopy = self.objectList
+
+                self.menuButtonAreaStyleUpdate(objectListCopy, False)
+                del self.objectList[:]              # wyczyszczenie listy obiektów
                 self.lastButtonObject = widget
-                changeButton = True
-            else:
-                changeButton = False
 
-        if changeButton == True:
-            self.menuButtonAreaStyleUpdate(widget, True)
-            self.menuButtonAreaStyleUpdate(iconObject, True)
-            self.menuButtonAreaStyleUpdate(textObject, True)
 
-    def menuButtonAreaStyleUpdate(self, widget, ischanged):
-        widget.setProperty('chosen', ischanged)
-        widget.style().unpolish(widget)
-        widget.style().polish(widget)
-        widget.update()
+    def menuButtonAreaStyleUpdate(self, objectList, ischanged):
+        for widget in objectList:
+            widget.setProperty('chosen', ischanged)
+            widget.style().unpolish(widget)
+            widget.style().polish(widget)
+            widget.update()
+
