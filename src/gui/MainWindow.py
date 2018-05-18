c433d820808dc7ea927765d6b7de3c6a4a5e7dbc
karol <karolsiegied@gmail.com>
2018-05-19 00:12:23 +0200

    Poprawiona numeracja stron

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -6,12 +6,12 @@ clicked = False
 
 class GUI_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super(GUI_MainWindow, self).__init__(parent) #QtCore.Qt.FramelessWindowHint
+        super(GUI_MainWindow, self).__init__(parent)       #QtCore.Qt.FramelessWindowHint
         self.setupUi(self)
         self.functionButtons()
-        self.lastButtonObject = QtWidgets.QFrame() # przechowuje obiekt poprzedniego przycisku menu
+        self.lastButtonObject = QtWidgets.QFrame()         # przechowuje obiekt poprzedniego przycisku menu
         self.objectList = []
-
+        self.pageMap = {'vfMain': 0, 'vfAlerts': 1, 'vfStats': 2, 'vfSettings': 3}
 
     def functionButtons(self):
         self.driveButton.pressed.connect(lambda: self.menuButtonChanged(self.vfMain))
@@ -30,9 +30,13 @@ class GUI_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         if not self.lastButtonObject.objectName():
             self.lastButtonObject = widget
+            print(widget.objectName())
+            self.setNewPage(self.pageMap[widget.objectName()])         # zmien stronę
             self.menuButtonAreaStyleUpdate(objectListCopy, True)
         else:
             if self.lastButtonObject.objectName() != widget.objectName():
+                print(widget.objectName())
+                self.setNewPage(self.pageMap[widget.objectName()])     # zmien stronę
                 self.menuButtonAreaStyleUpdate(objectListCopy, True)
                 del self.objectList[:]              # wyczyszczenie listy obiektów
 
@@ -55,3 +59,6 @@ class GUI_MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             widget.style().polish(widget)
             widget.update()
 
+
+    def setNewPage(self, index):
+        self.stackedWidget.setCurrentIndex(index)    # ustaw stronę
